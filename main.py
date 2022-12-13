92e5a8c2e89ba95d04f8db44de82fd3444ef85c0
Derrick McMillen <derrick@niche.club>
2022-12-13 09:24:38 -0500

    Added comment

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ print("-----------------")
 mp3 = DFPlayer(1, 4, 5)
 #mp3.EnableLoopAll()
 
+# Get number of files saved to / of sd card
 count = mp3.GetFilesCount()
 print("File count: " + str(count))
 
+# Pick a random file index
 file_index = randint(0, count)
 print("Playing: " + str(file_index));
 
+# Play file
 mp3.PlaySpecific(file_index)
